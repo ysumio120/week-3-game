42c8b5560a74baa3496a1b6992cbed23cff71cfa
Yuuta Sumioka <ysumio120@yahoo.com>
2016-08-18 00:40:59 -0400

    Changed theme to Pokemon, Finished trasnitions (auto restart)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,39 @@
-var list = ["Pinocchio", "Snow White", "Cinderella", "Woody", "Buzz Lightyear", "Winnie the Pooh",
-			"Tarzan", "Peter Pan", "Aladdin", "Belle", "Dumbo", "Hercules", "Quasimodo", "Baloo",
-			"Simba", "Mufasa", "Ariel", "Ursula", "Mickey Mouse"];
+var pokemon =
+	["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle",
+	"Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto",
+	"Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew",
+	"Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix",
+	"Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", 
+	"Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey",
+	"Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop",
+	"Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler",
+	"Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio",
+	"Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee",
+	"Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee",
+	"Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea",
+	"Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir",
+	"Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte",
+	"Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair",
+	"Dragonite", "Mewtwo", "Mew"];
+console.log(pokemon.length);
+
+// var list = ["Pinocchio", "Snow White", "Cinderella", "Woody", "Buzz Lightyear", "Winnie the Pooh",
+// 			"Tarzan", "Peter Pan", "Aladdin", "Belle", "Dumbo", "Hercules", "Quasimodo", "Baloo",
+// 			"Simba", "Mufasa", "Ariel", "Ursula", "Mickey Mouse"];
 var wins = 0;
+var losses = 0;
 start_game();
 
 //Game object
 function game() {
-	this.currentWord = list[Math.floor(Math.random() * list.length)];
+	this.currentWord = pokemon[Math.floor(Math.random() * pokemon.length)];
 	this.trackWord = blank(this.currentWord);
 	this.correctLetters = [];
 	this.guessedLetters = [];
-	this.guessesLeft = 10;
+	this.guessesLeft = 11;
 	this.guessedAlready = false;
-	this.correct = false;	
+	this.correct = false;
+	resetLives(this.guessesLeft);	
 }
 
 //When game starts, each letter of the chosen work is replaced with '_'
@@ -21,6 +42,10 @@ function blank(word) {
 		for(var i = 0; i < word.length; i++) {
 			if(word.charAt(i) == " ")
 				start.push(" ");
+			else if(word.charAt(i) == ".")
+				start.push(".")
+			else if(word.charAt(i) == "'")
+				start.push("'");
 			else
 				start.push("_");
 		}
@@ -40,12 +65,13 @@ function displayGuessesLeft(game) {
 
 //Displays the list of incorrect letters guessed
 function displayGuessedLetter(game){
-	document.getElementById("guessed").innerHTML = "Guessed: " + game.guessedLetters.join(", ");
+	document.getElementById("guessed").innerHTML = "Guessed:<br> " + game.guessedLetters.join(", ");
 }
 
 //Displays number of games won (Reset when refreshed)
 function displayWins() {
 	document.getElementById("numWins").innerHTML = "Wins: " + wins;
+	document.getElementById("numLosses").innerHTML = "Losses: " + losses;
 }
 
 //Retrieves the user's input (keystroke)
@@ -54,10 +80,40 @@ function getInput(event) {
 	return input;
 }
 
+function resetLives(num) {
+	for(var i = num-1; i >= 1; i--) {
+		var string = "pokeball_" + i;
+		console.log(string);
+		document.getElementById(string).setAttribute("style", "-webkit-filter : grayscale(0%)");
+	}
+}
+
+function timer() {
+	var newGame;
+	var time = 6;
+	var reset = setInterval(function() {
+					document.getElementById("timer").textContent = "New game starts in..." + --time;
+				
+					if(time <= 0) {
+						document.getElementById("win_lose").innerHTML = "";
+						clearTimeout(reset);
+						document.getElementById("question").style.display = "initial";
+						document.getElementById("char_pic").style.display = "none";
+						document.getElementById("character_name").innerHTML = "";
+						document.getElementById("timer").textContent = "";
+						newGame = new game();
+						play_game(newGame);
+					}
+
+				}, 1000);
+
+}
+
 //Starts playing game and any valid input starts couting as a guess
 function play_game(newGame) {
+
 	displayWord(newGame);
-	displayGuessesLeft(newGame);
+	//displayGuessesLeft(newGame);
 	displayGuessedLetter(newGame);
 	document.onkeypress = function() {
 		var input = getInput(event); 
@@ -99,33 +155,43 @@ function play_game(newGame) {
 				else if(!newGame.correct) {
 					newGame.guessedLetters.push(input);
 					newGame.guessesLeft--;
+					console.log(newGame.guessesLeft);
+					document.getElementById("pokeball_" + newGame.guessesLeft).setAttribute("style", "-webkit-filter : grayscale(100%)");
 					//If remaining number of guesses reaches zero, restart game with new word
-					if(newGame.guessesLeft == 0) {
-						document.getElementById("win_lose").innerHTML = "YOU LOSE!";
+					if(newGame.guessesLeft == 1) {
+						document.getElementById("win_lose").innerHTML = newGame.currentWord + " ran away!";
+						losses++;
+						displayWins();
 						displayCharacter(newGame.currentWord);
 						characterPicture(newGame.currentWord);
-						document.getElementById("char_pic").style.visibility = "visible";
-						newGame = new game();
-						play_game(newGame);
+						document.getElementById("question").style.display = "none";
+						document.getElementById("char_pic").style.display = "initial";
+						document.onkeypress = function() {}
+						timer();
 					}
-					displayGuessesLeft(newGame);
+					else {
+					//displayGuessesLeft(newGame);
+					//document.getElementById("pokeball_" + newGame.guessesLeft).setAttribute("style", "-webkit-filter : grayscale(100%)");
 					displayGuessedLetter(newGame);
+					}
 				}
 			}
 		}
 		//Increment number of wins and restart game with new word
 		if(newGame.currentWord == newGame.trackWord.join("")) {
-			document.getElementById("win_lose").innerHTML = "YOU WIN!";
+			document.getElementById("win_lose").innerHTML = newGame.currentWord + " was caught!";
 			wins++;
 			displayWins();
 			displayCharacter(newGame.currentWord);
 			characterPicture(newGame.currentWord);
-			document.getElementById("char_pic").style.visibility = "visible";
-			newGame = new game();
-			play_game(newGame);
+			document.getElementById("question").style.display = "none";
+			document.getElementById("char_pic").style.display = "initial";
+			document.onkeypress = function() {}
+			timer();
 		}
  	};
-}
+}	
+
 
 //Initiates game after pressing any key
 function start_game() {
@@ -137,7 +203,7 @@ function start_game() {
 		document.getElementById("start").style.display = "none";
 		console.log(input);
 		displayWord(newGame);
-	 	displayGuessesLeft(newGame);
+	 	//displayGuessesLeft(newGame);
 	 	displayWins();
 		play_game(newGame);
 	};
@@ -151,65 +217,76 @@ function displayCharacter(word) {
 //Load corresponding picture to chosen word (after win/loss)
 function characterPicture(word) {
 	var picture = document.getElementById("char_pic");
-	switch(word) {
-
-		case "Pinocchio":
-			picture.setAttribute("src", "assets/images/pinocchio.gif");
-			break;
-		case "Snow White":
-			picture.setAttribute("src", "assets/images/snow_white.png");
-			break;
-		case "Cinderella":
-			picture.setAttribute("src", "assets/images/Cinderella.jpg");
-			break;
-		case "Woody":
-			picture.setAttribute("src", "assets/images/woody.jpg");
-			break;
-		case "Buzz Lightyear":
-			picture.setAttribute("src", "assets/images/buzz_lightyear.png");
-			break;
-		case "Winnie the Pooh":
-			picture.setAttribute("src", "assets/images/winnie_the_pooh.jpg");
-			break;
-		case "Tarzan":
-			picture.setAttribute("src", "assets/images/tarzan.jpg");
-			break;
-		case "Peter Pan":
-			picture.setAttribute("src", "assets/images/peter_pan.jpeg");
-			break;
-		case "Aladdin":
-			picture.setAttribute("src", "assets/images/aladdin.png");
-			break;
-		case "Belle":
-			picture.setAttribute("src", "assets/images/belle.jpg");
-			break;
-		case "Dumbo":
-			picture.setAttribute("src", "assets/images/dumbo.jpg");
-			break;
-		case "Hercules":
-			picture.setAttribute("src", "assets/images/hercules.jpg");
-			break;
-		case "Quasimodo":
-			picture.setAttribute("src", "assets/images/quasimodo.jpg");
-			break;
-		case "Baloo":
-			picture.setAttribute("src", "assets/images/baloo.jpg");
-			break;
-		case "Simba":
-			picture.setAttribute("src", "assets/images/simba.png");
-			break;
-		case "Mufasa":
-			picture.setAttribute("src", "assets/images/mufasa.png");
-			break;
-		case "Ariel":
-			picture.setAttribute("src", "assets/images/ariel.png");
-			break;
-		case "Ursula":
-			picture.setAttribute("src", "assets/images/ursula.png");
-			break;
-		case "Mickey Mouse":
-			picture.setAttribute("src", "assets/images/mickey_mouse.jpg");
-			break;
-		default:;
+	if(word == "Mr. Mime")
+		word = "Mr._Mime";
+	if(word == "Nidoran") {
+		var rand = Math.floor(Math.random() * 2);
+		if(rand == 0)
+			word = "Nidoran_M";
+		else
+			word = "Nidoran_F";
 	}
+
+	picture.setAttribute("src", "assets/images/" + word + ".png");
+	// switch(word) {
+
+	// 	case "Pinocchio":
+	// 		picture.setAttribute("src", "assets/images/pinocchio.gif");
+	// 		break;
+	// 	case "Snow White":
+	// 		picture.setAttribute("src", "assets/images/snow_white.png");
+	// 		break;
+	// 	case "Cinderella":
+	// 		picture.setAttribute("src", "assets/images/Cinderella.jpg");
+	// 		break;
+	// 	case "Woody":
+	// 		picture.setAttribute("src", "assets/images/woody.jpg");
+	// 		break;
+	// 	case "Buzz Lightyear":
+	// 		picture.setAttribute("src", "assets/images/buzz_lightyear.png");
+	// 		break;
+	// 	case "Winnie the Pooh":
+	// 		picture.setAttribute("src", "assets/images/winnie_the_pooh.jpg");
+	// 		break;
+	// 	case "Tarzan":
+	// 		picture.setAttribute("src", "assets/images/tarzan.jpg");
+	// 		break;
+	// 	case "Peter Pan":
+	// 		picture.setAttribute("src", "assets/images/peter_pan.jpeg");
+	// 		break;
+	// 	case "Aladdin":
+	// 		picture.setAttribute("src", "assets/images/aladdin.png");
+	// 		break;
+	// 	case "Belle":
+	// 		picture.setAttribute("src", "assets/images/belle.jpg");
+	// 		break;
+	// 	case "Dumbo":
+	// 		picture.setAttribute("src", "assets/images/dumbo.jpg");
+	// 		break;
+	// 	case "Hercules":
+	// 		picture.setAttribute("src", "assets/images/hercules.jpg");
+	// 		break;
+	// 	case "Quasimodo":
+	// 		picture.setAttribute("src", "assets/images/quasimodo.jpg");
+	// 		break;
+	// 	case "Baloo":
+	// 		picture.setAttribute("src", "assets/images/baloo.jpg");
+	// 		break;
+	// 	case "Simba":
+	// 		picture.setAttribute("src", "assets/images/simba.png");
+	// 		break;
+	// 	case "Mufasa":
+	// 		picture.setAttribute("src", "assets/images/mufasa.png");
+	// 		break;
+	// 	case "Ariel":
+	// 		picture.setAttribute("src", "assets/images/ariel.png");
+	// 		break;
+	// 	case "Ursula":
+	// 		picture.setAttribute("src", "assets/images/ursula.png");
+	// 		break;
+	// 	case "Mickey Mouse":
+	// 		picture.setAttribute("src", "assets/images/mickey_mouse.jpg");
+	// 		break;
+	// 	default:;
+	// }
 }
